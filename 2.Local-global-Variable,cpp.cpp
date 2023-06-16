bc188c82e9ad6215bbab74d2b9eada7283945baa
Laxman <laxmanhenricks94@gmail.com>
2023-06-16 22:25:12 +0530

    Unassigned local and Global Variable Eg. line 10 && 13

---
+++ 2.Local-global-Variable,cpp.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int y=10,z; // GLobal Variable 
+
+
+int main(){
+	
+   int w, x=90; // Local Variable 
+   
+	cout<<"W is :"<<w<<endl; //   unassigned Local Varaible --> Garbage Value
+	cout<<"x is :"<<x<<endl;
+	cout<<"Y is :"<<y<<endl;
+	cout<<"Z is :"<<z<<endl; // Benefit of unassigned Global Varaible --> 0 Zero
+	
+		
+		
+}
