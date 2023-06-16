945ea7411413a95a617811fe9e287a0376e1b640
Laxman <laxmanhenricks94@gmail.com>
2023-06-16 22:07:33 +0530

    Size of opertor

---
+++ 1.SizeOf.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	
+//	Size of operator is used to return number of bytes stored by particulat data type
+
+	cout<<"Size of int  : "<<sizeof(int);
+	cout<<"\nSize of Char : "<<sizeof(char);
+	cout<<"\n Size of Long : "<<sizeof(long);
+	cout<<"\n********Long and int have 4 bytes**************";
+	cout<<"\n Size of Long Long : "<<sizeof(long long);
+	cout<<"\n Size of Float : "<<sizeof(float);
+	
+	
+	
+}
