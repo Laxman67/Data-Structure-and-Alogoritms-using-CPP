b726bc2dcf6f8c6a792d0f577ca7be2e75242e79
Laxman <laxmanhenricks94@gmail.com>
2023-06-17 22:32:18 +0530

    Special Concepts of Preprocessor directive

---
+++ 1.SizeOf.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	
+//	Size of operator is used to return number of bytes stored by particulat data type
+
+	cout<<"Size of int  : "<<sizeof(int);
+	cout<<"\nSize of Char : "<<sizeof(char);
+	cout<<"\n Size of Long : "<<sizeof(long);
+	cout<<"\n********Long and int have 4 bytes**************";
+	cout<<"\n Size of Long Long : "<<sizeof(long long);
+	cout<<"\n Size of Float : "<<sizeof(float);
+	
+	int x=10;S
+	cout<<"\n";
+	cout<<sizeof(x++)<<"\n"; // 4 Beacuse Sizeof is a compile time operator
+	cout<<"x is :"<<x;
+	
+	
+	
+}
---
+++ Special Concepts --Article/Macro1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+#define MAX 1
+#define MIN 100
+
+int main(){
+int x;
+
+cout<<"Maximum is ->"<<MAX<<endl;
+cout<<"Minimum is ->"<<MIN;
+cout<<"Enter Value :=";
+cin>>x;
+
+
+
+}
