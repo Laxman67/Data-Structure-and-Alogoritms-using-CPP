9703f64a4c2da961102d192bf105026a9352e36f
Laxman <laxmanhenricks94@gmail.com>
2023-06-19 22:34:57 +0530

    Fromatted

--- Intro-Variable-operator/6-Auto.cpp
+++ Intro-Variable-operator/6-Auto.cpp
@@ -8,9 +8,10 @@ int main()
 
     auto a = 10;
     auto b = 15.5;
-    cout<<a<<"\n"<<b<<"\n";
-    cout << typeid(a).name()<<"\n"
-    <<typeid(b).name();
+    cout << a << "\n"
+         << b << "\n";
+    cout << typeid(a).name() << "\n"
+         << typeid(b).name();
 
     return 0;
 }
