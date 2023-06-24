b6e20b44d6dab51f9be5a80b3f43e9ae91f52ca9
Laxman <laxmanhenricks94@gmail.com>
2023-06-24 21:30:12 +0530

    Note added to 12.Arithmetic

--- Intro-Variable-operator/12-Aritmetic-operator.cpp
+++ Intro-Variable-operator/12-Aritmetic-operator.cpp
@@ -4,6 +4,7 @@ int main()
 {
 
     // Note: The sign of a%b is same as sign of a --> first Opertor
+    //  In cazase of integer the result if a /b and a%b is undefined when b is 0
 
     int x = -20, y = 10;
 
