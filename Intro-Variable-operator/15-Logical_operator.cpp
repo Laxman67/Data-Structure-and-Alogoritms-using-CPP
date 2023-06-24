967a684f2feac03c28bafc610e452b7496bb72dd
Laxman <laxmanhenricks94@gmail.com>
2023-06-24 21:59:20 +0530

    15-Logical Operator

---
+++ Intro-Variable-operator/15-Logical_operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int x = 10, y = 20;
+
+    bool res = (x < 0 && x > 100);
+    cout << res << "\n";
+
+    res = (x > 0 || x > y);
+    cout << res << "\n";
+
+    res != res;
+    cout << res << "\n";
+}
