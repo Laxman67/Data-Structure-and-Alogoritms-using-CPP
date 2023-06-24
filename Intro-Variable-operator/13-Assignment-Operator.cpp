0ed0dcccba313c00fbdc02034b079a4f1981bda1
Laxman <laxmanhenricks94@gmail.com>
2023-06-24 21:42:00 +0530

    13-Assignment Operator

---
+++ Intro-Variable-operator/13-Assignment-Operator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    //  Assigment Opertor  --> Simple Assigment and Compound Assigment
+
+    int x = 10, y;
+
+    y = x;
+
+    cout << y << "\n";
+    x += y;
+    cout << x << "\n";
+    x -= 10;
+    cout << x << "\n";
+    x*=2;
+    cout << x << "\n";
+}
