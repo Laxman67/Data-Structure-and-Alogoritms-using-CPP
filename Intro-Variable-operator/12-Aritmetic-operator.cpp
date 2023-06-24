c6f0d6ace3c51589a775592e5b12e9e3fc45be30
Laxman <laxmanhenricks94@gmail.com>
2023-06-24 21:37:01 +0530

    Post-Fix and Pre-Fix

--- Intro-Variable-operator/12-Aritmetic-operator.cpp
+++ Intro-Variable-operator/12-Aritmetic-operator.cpp
@@ -5,13 +5,31 @@ int main()
 
     // Note: The sign of a%b is same as sign of a --> first Opertor
     //  In case of integer the result if a /b and a%b is undefined when b is 0
-    //  % opertor works for integer types only 
+    //  % opertor works for integer types only
 
-    int x = -20, y = 10;
+    //  BINARY OPERATOR
+
+    int x = -20, y = 10, z = 90;
 
     cout << (x + y) << "\n"
          << (x = y) << "\n"
          << (x % y) << "\n"
          << (x * y);
+
+    // UNARY OPERATOR
+
+    // ***** Postfix ***********//
+    // z++
+    // z--
+
+    cout << "\n " << z++;
+    
+
+    // ***** Prefix ***********//
+    //  ++z
+    // --z
+
+    cout << "\n " << ++z;
+
     return 0;
 }
