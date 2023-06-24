6bc57948b80002686a994d01f3ef450e332ab920
Laxman <laxmanhenricks94@gmail.com>
2023-06-24 22:02:11 +0530

    Name cam be used in place of Logical Opetor like and, or, not

--- Intro-Variable-operator/15-Logical_operator.cpp
+++ Intro-Variable-operator/15-Logical_operator.cpp
@@ -5,12 +5,16 @@ int main()
 
     int x = 10, y = 20;
 
-    bool res = (x < 0 && x > 100);
+    // bool res = (x < 0 && x > 100);
+    bool res = (x < 0 and x > 100);
+
     cout << res << "\n";
 
-    res = (x > 0 || x > y);
+    // res = (x > 0 || x > y);
+    res = (x > 0 or x > y);
     cout << res << "\n";
 
     res != res;
+    // res not= res;
     cout << res << "\n";
 }
