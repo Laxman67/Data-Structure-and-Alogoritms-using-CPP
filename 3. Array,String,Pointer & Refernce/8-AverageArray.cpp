a7a6873d7bf8e8c68136d03b9fc546f0c3d59d55
laxman67 <laxmanhenricks94@gmail.com>
2023-07-08 09:08:42 +0530

    Problem on Array

---
+++ 3. Array,String,Pointer & Refernce/8-AverageArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int averag_Array(int arr[], int n)
+{
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++)
+
+    {
+        ans = ans + arr[i];
+    }
+    return ans / n;
+}
+int main()
+{
+
+    int arr[]{10,20,30,40};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int sum = averag_Array(arr, n);
+
+    cout << sum;
+}
