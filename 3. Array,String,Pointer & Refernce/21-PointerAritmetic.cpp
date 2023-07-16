990f4a4f30b04d833c9b221ff516da31c9ad94fc
laxman67 <laxmanhenricks94@gmail.com>
2023-07-16 23:04:58 +0530

    Practise Problem on Pointer And Aritmetic

---
+++ 3. Array,String,Pointer & Refernce/21-PointerAritmetic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int arr[]{10, 20, 30, 60, 90};
+    int *ptr = arr;
+    int *ptr2 = arr;
+    ptr2 = ptr + 5;
+    cout << *ptr << endl;
+    ptr++;
+    cout << *ptr << endl;
+    ptr--;
+    cout << *ptr << endl;
+    ptr = ptr + 2;
+    cout << *ptr << endl;
+    // This will return number of object btw them
+    cout << (ptr2 - ptr);
+}
---
+++ 3. Array,String,Pointer & Refernce/22-Practise1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // int arr[]{10, 20, 30};
+    // int *ptr = arr;
+    // cout << sizeof(arr) << endl;
+    // cout << sizeof(ptr);
+
+    //  Char
+    char cha[]{'a', 'b', 'c'};
+    char *ptr = cha;
+    cout << sizeof(ptr) << endl;
+    cout << sizeof(ptr);
+}
